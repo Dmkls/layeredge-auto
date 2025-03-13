78ef9965ca9375b878fa5d21466a87d05e28ad09
My event horizon <88778695+MOTOR92@users.noreply.github.com>
2025-03-13 15:53:54 +0500

    add tasks.py

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ from configs import config
 from core import register
 from core import farm
 from core import db
-
+import tasks
 
 async def main():
-  Console().build()
-  await db.create_database()
-  tasks = []
+    Console().build()
+    await db.create_database()
+    task_list = []
 
-  if config.REGISTER_MODE:
-    tasks.append(asyncio.create_task(register.start()))
+    if config.REGISTER_MODE:
+        task_list.append(asyncio.create_task(register.start()))
 
-  if config.FARM_MODE:
-    tasks.append(asyncio.create_task(farm.start()))
+    if config.FARM_MODE:
+        task_list.append(asyncio.create_task(farm.start()))
 
-  if tasks:
-    await asyncio.gather(*tasks)
+    if config.TASKS_MODE:
+        task_list.append(asyncio.create_task(tasks.start()))
 
+    if task_list:
+        await asyncio.gather(*task_list)
 
 asyncio.run(main())
