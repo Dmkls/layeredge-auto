3fc79907408a27422a06cf0a66e84d751f1bd7f4
My event horizon <88778695+MOTOR92@users.noreply.github.com>
2025-03-13 15:54:27 +0500

    add connect_twitter

--- tasks.py
+++ tasks.py
@@ -2,22 +2,27 @@ import asyncio
 from random import randint
 from fake_useragent import UserAgent
 
-from core.reqs import send_prof, submit_prof, submit_light_node, submit_free_pass, submit_og_pass
-from utils.file_utils import read_proxies, read_wallets_to_complete_tasks
+from core.reqs import send_prof, submit_prof, submit_light_node, submit_free_pass, submit_og_pass, connect_twitter
+from utils.file_utils import read_proxies, read_wallets_to_complete_tasks, read_twitter_tokens
 from utils.private_key_to_wallet import private_key_to_wallet
 from utils.file_utils import write_failed_tasks, write_success_tasks
 from utils.log_utils import logger
 from core.account import Account
 from core import db
 from configs import config
+import asyncio
+import sys
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 PRIVATE_KEYS_TO_COMPLETE_TASKS = read_wallets_to_complete_tasks()
 PROXIES = read_proxies()
+TWITTER_TOKENS = read_twitter_tokens()
 ua_faker = UserAgent()
 write_failed_tasks('------------------------------------------------')
 write_success_tasks('------------------------------------------------')
 
-async def complete_tasks(private_key: str, proxy):
+async def complete_tasks(private_key: str, proxy, twitter_tokens):
     ua = await db.get_ua(private_key_to_wallet(private_key))
 
     if not ua:
@@ -43,12 +48,15 @@ async def complete_tasks(private_key: str, proxy):
     if config.DO_OG_PLEDGE_PASS_HOLD_TASK:
         await submit_og_pass(account, proxy)
         await asyncio.sleep(10, 30)
+    if config.DO_TWITTER_CONNECT:
+        await connect_twitter(account, proxy, twitter_tokens)
+        await asyncio.sleep(10, 30)
 
 async def start():
     await db.create_database()
     tasks = []
-    for private_key, proxy in zip(PRIVATE_KEYS_TO_COMPLETE_TASKS, PROXIES):
-        task = asyncio.create_task(complete_tasks(private_key, proxy))
+    for private_key, proxy, twitter_tokens in zip(PRIVATE_KEYS_TO_COMPLETE_TASKS, PROXIES, TWITTER_TOKENS):
+        task = asyncio.create_task(complete_tasks(private_key, proxy, twitter_tokens))
         tasks.append(task)
         await asyncio.sleep(0.1)
 
