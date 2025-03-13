560ded18331235d68e188c6947c9c71b16a0a86b
My event horizon <88778695+MOTOR92@users.noreply.github.com>
2025-03-13 15:56:14 +0500

    Update log_utils.py

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -1,5 +1,31 @@
 import os
+from sys import stderr
 from loguru import logger
-from configs.constants import LOG_DIR
+from configs.config import LOG_LEVEL, LOG_FILE
 
-logger.add(os.path.join(LOG_DIR, 'log.log'), format='{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}', level='DEBUG')
+os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+
+logger.remove()
+
+logger_format = (
+    "<cyan>{time:YYYY-MM-DD HH:mm:ss}</cyan> | "
+    "<level>{level: ^8}</level> | "
+    "<cyan>{function:^15}</cyan> | "
+    "<level>{message}</level>"
+)
+
+logger.add(
+    sink=stderr,
+    format=logger_format,
+    level=LOG_LEVEL,
+    colorize=True
+)
+
+logger.add(
+    sink=LOG_FILE,
+    format=logger_format,
+    level=LOG_LEVEL,
+    rotation="15 MB",
+    retention="7 days",
+    enqueue=True
+)
